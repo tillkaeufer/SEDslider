7afd0eb3e7af90888916ddfc73d1703bbd3849fc
Till Kaeufer <tfk1@st-andrews.ac.uk>
2021-08-25 13:26:14 +0100

    chi and parameter.in

---
+++ chi_computations.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def chi_window(Fmod,Fphot,sphot,lphot):
+    #--------- compute chi in spectral windows --------
+    window = [0.3,1.0,3.0,8.0,15.0,30.0,100.0,300.0,1000.0,3000.0,1.E+99]
+    iwin = 0
+    Nchi = 0
+    chi2 = 0.0
+    Nwin = 0
+    chi2win = 0.0 
+    Ndat=len(Fphot)
+    for i in range(0,Ndat):
+        dchi =  np.log(Fmod[i]/Fphot[i])/(sphot[i]/Fphot[i]) #is this really log or log10
+        if lphot[i]>window[iwin]:     # window finished
+            if iwin>0:
+#                print('%3d %3d %7.3f' % (iwin+1,Nwin,np.sqrt(chi2win/Nwin)))
+                chi2 += chi2win/Nwin 
+                Nchi += 1
+            #else:
+            #    print()
+
+            iwin += 1
+            Nwin = 0
+            chi2win = 0.0
+
+        chi2win += dchi**2            # add dchi to window-chi
+        Nwin += 1
+#        print('%13.5f %10.6e %10.6e %6.2f%c %7.2f' % (lphot[i],Fphot[i],Fmod[i],sphot[i]/Fphot[i]*100.0,"%",dchi))
+
+    #print(Nchi+1,Nwin,np.sqrt(chi2win/Nwin))
+    if Nwin>0:
+        chi2 += chi2win/Nwin          # last window
+        Nchi += 1
+
+    chi = np.sqrt(chi2/Nchi)        # total chi
+    #print(' ==>  chi = %8.4f' % (chi))
+    return chi
---
+++ para_transform.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def para_to_parameterin(input_file,output_file):
+    para_dict={}
+    with open(input_file,'r') as f:
+        lines=f.readlines()
+        for line in lines:
+            split_line=line.split()
+            value=float(split_line[0])
+            parameter=split_line[1]
+            para_dict[parameter]=value
+    with open('TemplateParameter.in','r') as f_temp:
+        lines=f_temp.readlines()
+    with open(output_file,'w') as f:
+        for line in lines:
+            if '!' in line:
+                idx=line.index('!')
+                #print(idx)
+                if 'dist' in line:
+                    value=para_dict['Dist[pc]']
+                    new_line=f'%12.5e {line[idx:]}' %value
+                else:
+                    for key in para_dict.keys():
+                        if key in line:
+                            value=para_dict[key]
+                            new_line=f'%12.5e {line[idx:]}' %value
+                            break
+                        else:
+                            new_line=line
+            else:
+                if 'Mg0.7Fe0.3SiO3[s]' in line:
+                    idx=line.index('Mg')
+                    new_line=f'%12.5e {line[idx:]}' %value
+                if 'amC-Zubko[s]' in line:
+                    idx=line.index('amC-Zubko[s]')
+                    new_line=f'%12.5e {line[idx:]}' %value
+                else:
+                    new_line=line
+            #print(new_line)
+            f.write(new_line)     
